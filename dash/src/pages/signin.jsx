e2dbb14972d0204f27979b4a88e886bd504d7b81
Abdalrhman mohamed <abdalhmanhmouda@gmail.com>
2025-12-04 10:24:46 +0200

    sign in completed

--- dash/src/pages/signin.jsx
+++ dash/src/pages/signin.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "./signin.css"
 import log from "../assets/log.svg"
+import Btn from '../component/btn';
 const Sign = () => {
     return ( <>
     <img className='img' src={log} />
@@ -8,8 +9,24 @@ const Sign = () => {
     <div>
         <div className='first'>First Name</div>
         <div className='rec2'></div>
+        <div className='last'>Last Name</div>
         <div className='rec3'></div>
     </div>
+    <div>
+        <div className='mail'>Email</div>
+        <div className='rec4'></div>
+    </div>
+    <div>
+        <div className='pass'>Password</div>
+        <div className='rec5'></div>
+    </div>
+    <div>
+        <div className='comf'>Comfirm Password</div>
+        <div className='rec6'></div>
+    </div>
+    <Btn />
+    
+    <div className='you'>You already have an account Log in</div>
     </> );
 }
  
